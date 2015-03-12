39f4a255a460e578170df66adecd56ff2b9c87f5
morelandjs <moreland@localhost.localdomain>
2015-03-12 17:27:14 -0400

    semi-final

--- splines/splines.py
+++ splines/splines.py
@@ -3,9 +3,12 @@ import numpy as np
 import scipy 
 import matplotlib.pyplot as plt
 import prettyplotlib as ppl
+import fortranformat as ff
+from fortranformat import FortranRecordWriter
 from prettyplotlib import brewer2mpl
 from scipy import interpolate
 from scipy.interpolate import interp1d
+from scipy.interpolate import UnivariateSpline
 from scipy.optimize import fminbound
 from scipy import integrate
 from scipy.integrate import quad
@@ -20,8 +23,8 @@ from scipy.integrate import quad
 T, I, e, p, Cs = np.loadtxt("hrg-eos/hrg-urqmd-eos.dat",dtype='float',unpack=True,skiprows=1)
 fI_lo = interp1d(T, I, kind='cubic')
 
-T = np.linspace(5,200,100)
-ppl.plot(T/1000,fI_lo(T),'--',color='orange',linewidth=3.0,zorder = 301, label="HRG UrQMD partial reson")
+#T = np.linspace(5,200,100)
+#ppl.plot(T/1000,fI_lo(T),'--',color='orange',linewidth=3.0,zorder = 301, label="HRG UrQMD partial reson")
 ####################################################################################
 
 # construct high temperature interaction measure
@@ -65,8 +68,8 @@ for iT in T:
 fI_hi = interp1d(T*1000, I, kind='cubic')
 
 # plot HotQCD trace anomaly))
-T = np.linspace(130,800,1000)
-ppl.plot(T/1000,fI_hi(T),color="blue",linewidth=3.0,zorder = 99,label="HotQCD parameterization")
+#T = np.linspace(130,800,1000)
+#ppl.plot(T/1000,fI_hi(T),color="blue",linewidth=3.0,zorder = 99,label="HotQCD parameterization")
 
 #########################################################################################################################
 
@@ -86,7 +89,7 @@ k1, k4, k2, k3, C5, C6, C7, C8, C9, C10, C11, C12, C13, C14, C15, C16 = np.loadt
 N = k1.size
 
 # loop over splines (range fixed to 1 to test first interaction measure)
-for ic in range(50):
+for ic in range(5):
 
     # initialize basis functions
     B1 = np.zeros(nT)
@@ -118,8 +121,7 @@ for ic in range(50):
         for iT,T in enumerate(Tvec):
             if t[i] <= T and T < t[i+1]:
                 B[i][iT] = 1.0
-
-
+    
     # iterate recursive algorithm thrice B0 -> B1 -> B2 -> B3
     for k in range(1,4):
         for i in range(5):
@@ -130,27 +132,85 @@ for ic in range(50):
     Imid = C6[ic] + C5[ic]*(Tvec-130) + C7[ic]*B[1] + C8[ic]*B[2] + C9[ic]*B[3] + C10[ic]*B[4]
     fI_mid = interp1d(Tvec,Imid)  
 
-
     # determine high temperature switching parameters
     Td = np.linspace(350,400,1000)
     dfI_mid = interp1d(Td, interpolate.splev(Td,interpolate.splrep(Td,fI_mid(Td),s=0),der=1))
     dfI_hi = interp1d(Td, interpolate.splev(Td,interpolate.splrep(Td,fI_hi(Td),s=0),der=1))
     deriv = interp1d(Td,abs(dfI_hi(Td)-dfI_mid(Td)))
-    Tsw = fminbound(deriv,350,400) 
+    Tsw = fminbound(deriv,350,400)
+    shift = fI_mid(Tsw) - fI_hi(Tsw)
 
     # blend low, intermediate and high regions together
     for iT,T in enumerate(Tvec):
         if T < 120:
             I[iT] = fI_lo(T)
         elif T >= 120 and T < 180:
-            I[iT] = fI_lo(T) + (1.+np.tanh((T-150)/5))/2.*(fI_mid(T) - fI_lo(T))
+            w1 = lambda x: 5/(1+np.exp((x-176)/2))
+            u1 = lambda x: (1+np.tanh((T-150)/w1(T)))/2
+            I[iT] = fI_lo(T) + u1(T)*(fI_mid(T)-fI_lo(T))
         elif T >= 180 and T < 500:
-            I[iT] = fI_mid(T) + (1.+np.tanh((T-Tsw)/20))/2.*(fI_hi(T) + fI_mid(Tsw) - fI_hi(Tsw) - fI_mid(T))
+            w2 = lambda x: 40*(1/(1+np.exp(-(x-190)/5)) * 1/(1+np.exp((x-490)/5)))
+            u2 = lambda x: (1+np.tanh((T-Tsw)/w2(T)))/2
+            I[iT] = fI_mid(T) + u2(T)*(fI_hi(T) + shift - fI_mid(T))
         else:
-            I[iT] = fI_hi(T) + fI_mid(Tsw) - fI_hi(Tsw)     	
+            I[iT] = fI_hi(T) + shift  
+
+    fI = interp1d(Tvec,I)   	            
+    
+    # calculate p/T**4 from interaction measure
+    p = []
+    integral = 0.0
+    for iT,T in enumerate(Tvec):
+        p.append(integral)
+        integral += I[iT]/T*dT
+    fp = interp1d(Tvec,p)
+   
+    # calculate e/T**4 
+    e = []
+    for iT,T in enumerate(Tvec):
+        e.append(I[iT] + 3.*p[iT])
+    fe = interp1d(Tvec,e)
+
+    # calculate s/T**3
+    s = []
+    for iT,T in enumerate(Tvec):
+        s.append(e[iT] + p[iT])
+    fs = interp1d(Tvec,s)
+
+    ppl.plot(Tvec,e,label="energy")
+    ppl.plot(Tvec,p,label="pressure")
+    ppl.plot(Tvec,s,label="entropy")
+    #continue   
+
+    # e output mesh: GeV/fm^3
+    emin = 0.1e-2
+    emax = 0.310999e3
+    #ne = 155500
+    ne = 100
+    de = (emax-emin)/float(ne)
+    evec = np.linspace(emin,emax,ne)
+
+    # T output mesh: GeV
+    Tinv = []
+    for ie0,e0 in enumerate(evec):
+        Tinv.append(fminbound(lambda x: abs(fe(x)*(x/1000)**4./(hbarc**3.)-e0),Tmin,Tmax))
+    Tinv = np.asarray(Tinv)
+
+    # output arrays
+    eEOS = fe(Tinv)*Tinv**4./(hbarc**3.)
+    pEOS = fp(Tinv)*Tinv**4./(hbarc**3.)
+    sEOS = fs(Tinv)*Tinv**3./(hbarc**3.)
+    TEOS = Tinv
+
+    line = FortranRecordWriter('(4E15.6)')
+
+    # open output file
+    with open("eos-tables/HotQCD-EOS-{}.dat".format(ic),'w') as wf:
+        for i in range(len(eEOS)):
+            wf.write(line.write([eEOS[i],pEOS[i],sEOS[i],TEOS[i]])+"\n")
 
     # plot that shit!
-    ppl.plot(Tvec/1000,I)
+    ppl.plot(Tvec/1000,p) 
 
     #dI = interp1d(Tvec, interpolate.splev(Tvec,interpolate.splrep(Tvec,I,s=0),der=2))
     #ppl.plot(Tvec/1000,dI(Tvec))
